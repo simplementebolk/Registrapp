38b012192a3aa27fe98ae2e2ba02bb376db239ef
Enzo Araya <en.araya@duocuc.cl>
2024-09-12 21:32:43 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Registrapp',
+  webDir: 'www'
+};
+
+export default config;
