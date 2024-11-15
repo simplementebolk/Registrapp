c365a99de049a9df3b0d775a8f9cc0ca7ea4e354
Nfx00q <babgutier@gmail.com>
2024-11-14 23:06:31 -0300

    cambios page profesor asistencia y bitacora

--- src/app/pages/profesor/bitacora/bitacora.page.ts
+++ src/app/pages/profesor/bitacora/bitacora.page.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BitacoraPage implements OnInit {
 
+
+  fecha: string;
+  resumen: string;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.fecha = '';
+    this.resumen = '';
+  }
+
+
+  guardarBitacora() {
+    if (this.fecha && this.resumen) {
+      console.log('Bitácora Guardada: ', { fecha: this.fecha, resumen: this.resumen });
+      alert('Bitácora guardada con éxito.');
+    } else {
+      alert('Por favor, complete todos los campos.');
+    }
   }
 
 }
